b9924574be720c7b92e8590373781ea70d0069a3
Ankitbaghel07 <ankitbaghel00007@gmail.com>
2025-07-12 19:11:16 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,6 @@
+# check number is odd or even
+a=int(input("enter a number:"))
+if(a%2 == 0):
+    print(f"{a} is a even number")
+else:
+    print(f"{a} is a odd number")
---
+++ task2.py
@@ -0,0 +1,5 @@
+#to add 1 to 50
+a=0
+for i in range(1,51):
+    a+=i
+print(f"the sum of numbers from 1 to 50 is: {a}")
